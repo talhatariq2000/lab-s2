d7d09d743d2de87800b9bdaa942b5884abdfac56
talhatariq2000 <76400023+talhatariq2000@users.noreply.github.com>
2021-05-18 15:56:45 +0500

    Create code.js

---
+++ code.js
@@ -0,0 +1,57 @@
+jQuery(document).ready(function ($) {
+
+    $('.prev').hide();
+
+    $.ajax({
+        type: "GET",
+        url: "https://reqres.in/api/users?page=" + 1,
+
+        success: function (res) {
+            $('.texts').html('');
+            for (var a = 0; a < res.data.length; a++) {
+                $('.texts').append(' <div class="single-data"> <p class="avatar"> <img src="' + res.data[a].avatar + '" alt=""> </p> <p class="id">' + res.data[a].id + '</p> <p class="firstname ">' + res.data[a].first_name + '</p> <p class="lastname ">' + res.data[a].last_name + '</p> <p class="email">' + res.data[a].email + '</p> </div>');
+            }
+
+        }
+    });
+
+
+
+    $('.next').click(function () {
+        $.ajax({
+            type: "GET",
+            url: "https://reqres.in/api/users?page=" + 2,
+
+            success: function (res) {
+                $('.texts').html('');
+                for (var a = 0; a < res.data.length; a++) {
+                    $('.texts').append(' <div class="single-data"> <p class="avatar"> <img src="' + res.data[a].avatar + '" alt=""> </p> <p class="id">' + res.data[a].id + '</p> <p class="firstname ">' + res.data[a].first_name + '</p> <p class="lastname ">' + res.data[a].last_name + '</p> <p class="email">' + res.data[a].email + '</p> </div>');
+                }
+
+            }
+        });
+
+
+        $('.next').hide();
+        $('.prev').show();
+    });
+
+    $('.prev').click(function () {
+        $.ajax({
+            type: "GET",
+            url: "https://reqres.in/api/users?page=" + 1,
+
+            success: function (res) {
+                $('.texts').html('');
+                for (var a = 0; a < res.data.length; a++) {
+                    $('.texts').append(' <div class="single-data"> <p class="avatar"> <img src="' + res.data[a].avatar + '" alt=""> </p> <p class="id">' + res.data[a].id + '</p> <p class="firstname ">' + res.data[a].first_name + '</p> <p class="lastname ">' + res.data[a].last_name + '</p> <p class="email">' + res.data[a].email + '</p> </div>');
+                }
+
+            }
+        });
+
+        $('.prev').hide();
+        $('.next').show();
+    });
+
+});
